c6cb3fbfe8f96894f449b7238163c3111eb1f332
Jirka <del@hs-furtwangen.de>
2020-04-22 17:49:04 +0200

    Notes and TypeScript foundation

---
+++ W18/Praktikum/L01/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace L01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hallo" + " " + args[0]);
+        }
+    }
+}
