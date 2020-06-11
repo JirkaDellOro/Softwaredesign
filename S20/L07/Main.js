d4c83d2c866159a35642eb5b0af8aa6eb9bb56aa
Jirka Dell'Oro-Friedl <del@hs-furtwangen.de>
2020-06-11 10:03:16 +0200

    Save

--- S20/L07/Main.js
+++ S20/L07/Main.js
@@ -3,18 +3,37 @@ var L07_LoadJSON;
 (function (L07_LoadJSON) {
     class Data {
     }
-    console.log("Start load");
-    load("test.json");
-    console.log("Done load");
+    test();
+    async function test() {
+        console.log("Start load");
+        let content = await load("test.json");
+        console.log("Done load");
+        console.log(content);
+        console.log("Start load");
+        save(JSON.stringify(content), "test.json");
+        console.log("Done load");
+    }
     async function load(_filename) {
         console.log("Start fetch");
         let response = await fetch(_filename);
         let text = await response.text();
         let json = JSON.parse(text);
-        // let json: any = await response.json();
-        console.log(json);
-        console.log(json.array);
+        // alternative: json = await response.json();
         console.log("Done fetch");
+        return (json);
+    }
+    function save(_content, _filename) {
+        let blob = new Blob([_content], { type: "text/plain" });
+        let url = window.URL.createObjectURL(blob);
+        //*/ using anchor element for download
+        let downloader;
+        downloader = document.createElement("a");
+        downloader.setAttribute("href", url);
+        downloader.setAttribute("download", _filename);
+        document.body.appendChild(downloader);
+        downloader.click();
+        document.body.removeChild(downloader);
+        window.URL.revokeObjectURL(url);
     }
 })(L07_LoadJSON || (L07_LoadJSON = {}));
 //# sourceMappingURL=Main.js.map
--- S20/L07/Main.ts
+++ S20/L07/Main.ts
@@ -1,26 +1,47 @@
 namespace L07_LoadJSON {
 
   class Data {
-    array: {text: string}[];
+    array: { text: string }[];
   }
 
-  console.log("Start load");
-  load("test.json");
-  console.log("Done load");
+  test();
 
-  async function load(_filename: string): Promise<void> {
+  async function test(): Promise<void> {
+    console.log("Start load");
+    let content: Data = await load("test.json");
+    console.log("Done load");
+
+    console.log(content);
+
+    console.log("Start load");
+    save(JSON.stringify(content), "test.json");
+    console.log("Done load");
+  }
+
+  async function load(_filename: string): Promise<Data> {
     console.log("Start fetch");
 
     let response: Response = await fetch(_filename);
 
     let text: string = await response.text();
     let json: Data = JSON.parse(text);
-
-    // let json: any = await response.json();
-    
-    console.log(json);
-    console.log(json.array);
+    // alternative: json = await response.json();
 
     console.log("Done fetch");
+    return (json);
+  }
+
+  function save(_content: string, _filename: string): void {
+    let blob: Blob = new Blob([_content], { type: "text/plain" });
+    let url: string = window.URL.createObjectURL(blob);
+    //*/ using anchor element for download
+    let downloader: HTMLAnchorElement;
+    downloader = document.createElement("a");
+    downloader.setAttribute("href", url);
+    downloader.setAttribute("download", _filename);
+    document.body.appendChild(downloader);
+    downloader.click();
+    document.body.removeChild(downloader);
+    window.URL.revokeObjectURL(url);
   }
 }
